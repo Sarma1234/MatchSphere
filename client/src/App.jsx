42fc6383c3cdaa666092ea8c18a94d6b07e5df5f
Harshita <sarmaharshita44@gmail.com>
2026-07-14 15:58:44 +0530

    auth done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
-import LandingPage from "./pages/LandingPage/LandingPage";
+// import LandingPage from "./pages/LandingPage/LandingPage";
+
+// function App() {
+//   return <LandingPage />;
+// }
+
+// export default App;
+
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
