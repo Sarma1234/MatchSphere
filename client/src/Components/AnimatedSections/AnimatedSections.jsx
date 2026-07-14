e654f9e320722318d66bf9e3ef805e3dec665e27
Harshita <sarmaharshita44@gmail.com>
2026-07-14 12:24:44 +0530

    implemented landing page with all its components

---
+++ client/src/Components/AnimatedSections/AnimatedSections.jsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+
+function AnimatedSections({ children }) {
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+        y: 70,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+      }}
+      viewport={{
+        once: true,
+        amount: 0.25,
+      }}
+      transition={{
+        duration: 0.8,
+        ease: "easeOut",
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default AnimatedSection;
